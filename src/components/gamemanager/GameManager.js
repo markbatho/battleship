ab61b1d7122757addcd38c28611d7b550213bc35
Mark Batho <mark747488@gmail.com>
2023-04-26 01:24:51 +0200

    Implement player's ship placement ui base

---
+++ src/components/gamemanager/GameManager.js
@@ -0,0 +1,126 @@
+import Ship from '../ship/Ship';
+
+export default class GameManager {
+  constructor(player, computer, parent) {
+    this.player = player;
+    this.computer = computer;
+    this.parent = parent;
+    this.playerBoard = null;
+    this.computerBoard = null;
+  }
+
+  renderDeploymentBoard() {
+    const fleet = [
+      new Ship(2),
+      new Ship(3),
+      new Ship(3),
+      new Ship(4),
+      new Ship(5),
+    ];
+
+    const board = document.createElement('div');
+    board.className = 'board';
+
+    for (let i = 0; i < this.player.gameboard.size; i++) {
+      for (let j = 0; j < this.player.gameboard.size; j++) {
+        const cell = document.createElement('div');
+        cell.className = 'placement-cell';
+        cell.dataset.coord = [i, j];
+        board.appendChild(cell);
+      }
+    }
+
+    let activeCells = [];
+    let dir = 'W';
+
+    function selectCells(start, dir, length) {
+      for (let i = 0; i < length; i++) {
+        let nextCell;
+
+        if (dir === 'N') {
+          nextCell = document.querySelector(
+            `[data-coord="${+start.dataset.coord[0] - i},${+start.dataset
+              .coord[2]}"]`
+          );
+        } else if (dir === 'S') {
+          nextCell = document.querySelector(
+            `[data-coord="${+start.dataset.coord[0] + i},${+start.dataset
+              .coord[2]}"]`
+          );
+        } else if (dir === 'W') {
+          nextCell = document.querySelector(
+            `[data-coord="${+start.dataset.coord[0]},${
+              +start.dataset.coord[2] + i
+            }"]`
+          );
+        } else if (dir === 'E') {
+          nextCell = document.querySelector(
+            `[data-coord="${+start.dataset.coord[0]},${
+              +start.dataset.coord[2] - i
+            }"]`
+          );
+        }
+
+        if (nextCell) {
+          activeCells.push(nextCell);
+          nextCell.style.backgroundColor = '#ccc';
+        } else {
+          activeCells.forEach((elem) => (elem.style.backgroundColor = 'red'));
+          start.style.cursor = 'not-allowed';
+        }
+      }
+    }
+
+    function deselectCells() {
+      while (activeCells.length > 0) {
+        const elem = activeCells.pop();
+        elem.style.backgroundColor = '#f2f2f2';
+        elem.style.cursor = 'auto';
+      }
+    }
+
+    board.addEventListener('mouseover', (event) => {
+      if (event.target.classList.contains('placement-cell')) {
+        if (fleet.length > 0) {
+          selectCells(event.target, dir, fleet[fleet.length - 1].length);
+          console.log('called?');
+        }
+      }
+    });
+
+    board.addEventListener('mouseout', (event) => {
+      if (event.target.classList.contains('placement-cell')) {
+        deselectCells();
+      }
+    });
+
+    board.addEventListener('wheel', (event) => {
+      if (event.target.classList.contains('placement-cell')) {
+        if (fleet.length > 0) {
+          if (dir === 'N') dir = 'W';
+          else if (dir === 'S') dir = 'E';
+          else if (dir === 'W') dir = 'S';
+          else if (dir === 'E') dir = 'N';
+
+          deselectCells();
+          selectCells(event.target, dir, fleet[fleet.length - 1].length);
+        }
+      }
+    });
+
+    board.addEventListener('click', (event) => {
+      if (event.target.classList.contains('placement-cell')) {
+        if (fleet.length > 0) {
+          if (activeCells.length === fleet[fleet.length - 1].length) {
+            activeCells.forEach((elem) => (elem.className = 'ship'));
+            fleet.pop();
+            deselectCells();
+          }
+        }
+      }
+    });
+
+    this.parent.replaceChildren();
+    this.parent.appendChild(board);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
+import './style.css';
+
+import Gameboard from './components/gameboard/Gameboard';
+import Player from './components/player/Player';
+import GameManager from './components/gamemanager/GameManager';
+
 const app = document.getElementById('app');
-app.textContent = 'Hello, World!';
+const header = document.createElement('header');
+const boardContainer = document.createElement('div');
+const title = document.createElement('h1');
+title.textContent = 'Battleship';
+header.appendChild(title);
+app.appendChild(header);
+app.appendChild(boardContainer);
+
+const playerBoard = new Gameboard();
+const player = new Player(playerBoard);
+const computerBoard = new Gameboard();
+const computer = new Player(computerBoard);
+const gameManager = new GameManager(player, computer, boardContainer);
+
+gameManager.renderDeploymentBoard();
